029cfcf83867e9f779c3dd3596072b815715fa5c
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-27 21:01:48 +0200

    Added IWarehouse interface

---
+++ WarehouseLib/IWarehouse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseLib
+{
+    interface IWarehouse
+    {
+        bool HasProduct(string product);
+        int CurrentStock(string product);
+        void AddStock(string product, int amount);
+        void TakeStock(string product, int amount);
+    }
+}
