10b18d9d49719e6107d2c26b2ce9aeb8cee6422e
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-27 21:16:43 +0200

    added InsufficientStockException

---
+++ WarehouseLib/InsufficientStockException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseLib
+{
+    [Serializable]
+    class InsufficientStockException : Exception
+    {
+        public InsufficientStockException()
+        {
+
+        }
+        public InsufficientStockException(int amount)
+            : base(String.Format("Amount {0} exceeds current store", amount))
+        {
+
+        }
+    }
+}
