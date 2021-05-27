2efba45d36f0707f22e6533bc63feacd8f618cea
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-27 21:13:37 +0200

    added NoSuchProductException

---
+++ WarehouseLib/NoSuchProductException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseLib
+{
+    [Serializable]
+    class NoSuchProductException : Exception
+    {
+        public NoSuchProductException()
+        {
+
+        }
+        public NoSuchProductException(string product)
+            : base(String.Format("No such product: {0}", product))
+        {
+
+        }
+    }
+}
