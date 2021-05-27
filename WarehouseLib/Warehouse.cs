cb6d7851d69a6dd027ed2274361aab392f90bfd9
TheRealDealveloper <66743070+TheRealDealveloper@users.noreply.github.com>
2021-05-27 21:08:11 +0200

    added exception for invalid prodcut name input

--- WarehouseLib/Warehouse.cs
+++ WarehouseLib/Warehouse.cs
@@ -4,7 +4,42 @@ using System.Text;
 
 namespace WarehouseLib
 {
-    class Warehouse
+    class Warehouse : IWarehouse
     {
+        public void AddStock(string product, int amount)
+        {
+            if (String.IsNullOrWhiteSpace(product))
+            {
+                throw new ArgumentException();
+            }
+            throw new NotImplementedException();
+        }
+
+        public int CurrentStock(string product)
+        {
+            if (String.IsNullOrWhiteSpace(product))
+            {
+                throw new ArgumentException();
+            }
+            throw new NotImplementedException();
+        }
+
+        public bool HasProduct(string product)
+        {
+            if (String.IsNullOrWhiteSpace(product))
+            {
+                throw new ArgumentException();
+            }
+            throw new NotImplementedException();
+        }
+
+        public void TakeStock(string product, int amount)
+        {
+            if (String.IsNullOrWhiteSpace(product))
+            {
+                throw new ArgumentException();
+            }
+            throw new NotImplementedException();
+        }
     }
 }
